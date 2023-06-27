f0179c498ac82d6b1eed5f4fd5da73d6454ab861
Sarah Bouzidi <36506749+srh-bzd@users.noreply.github.com>
2023-06-27 16:28:27 +0200

    Add files via upload

---
+++ ViroScan/config/config.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+<< COMMENT
+
+    config.sh is a script belonging to ViroScan pipeline. This script contain the value by default of various args.
+
+    AUTHOR :
+        Sarah BOUZIDI
+        Engineer in bioinformatics
+        Centre National de la Recherche Scientifique (CNRS)
+        Team Virostyle, Laboratory MIVEGEC, IRD, Montpellier
+        
+COMMENT
+
+# Exit if a command exits with a non zero status
+set -e
+
+
+########################################################################################
+#                                     VARIABLES
+########################################################################################
+
+# Path to the directory of scripts
+export DIRSCRIPTS="$(dirname $0)"/src
+
+# Path to the directory of data
+export DIRDATA="$(dirname $0)"/data
+
+# Path to the reference indexed
+export REFINDEXED
+
+# Path of tools used
+export BOWTIE=bowtie2
+export BRESEQ=breseq
+export GDTOOLS=gdtools
+
+# breseq parameters
+export BRESEQ_OPTIONS="-p -t"
+
+# Threshold
+export THRESHOLD=5
+
+# Threads
+export THREADS=1
